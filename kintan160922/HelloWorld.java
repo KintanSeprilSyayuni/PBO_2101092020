2e0aa3b51dad1c039d1b8e5e41609aca3f611ccd
KintanSeprilSyayuni <114288995+KintanSeprilSyayuni@users.noreply.github.com>
2022-10-12 22:27:39 +0700

    Create HelloWorld.java

---
+++ kintan160922/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kintan160922;
+
+/**
+ *
+ * @author User
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World\n" + "Mi2c\n" + "pbo\n");  // komentar klo satu baris 
+        System.out.println("Nama\t: Kintan Sepri Syayuni\n" + "No.BP\t: 2101092020\n" + "Prodi\t: Manajemen Informatika\n" + 
+                "Jurusan\t: Teknologi Informasi\n" + "Alamat\t: JL. Alai Timur No.63");
+        
+    }
+
+}
